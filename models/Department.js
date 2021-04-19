eeb2846d3fe9b380fc768d7bb7fff2d1fb88b97a
Brandon King <brandonscottking@gmail.com>
2021-04-19 12:07:14 -0400

    wrote dept & role models

---
+++ models/Department.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Department extends Model {}
+
+Department.init(
+    {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: 'department',
+}
+);
+
+module.exports = Department;
