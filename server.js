8ae3df109114767ff2366e1398d2b89a4d122754
Brandon King <brandonscottking@gmail.com>
2021-04-22 17:05:10 -0400

    refactored code to not use express/sequelize

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 //set dependencies
-const express = require('express');
-const routes = require('./routes');
-const connection = require('./config/connection');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
-const app = express();
-const PORT = process.env.port || 3005;
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+//inquirer functions
+const mainMenu = () => {
+    inquirer
+    .prompt({
+        name: 'userOption',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: ['View all employees', 'View all departments', 'View all roles', 'Add employee', 'Add role', 'Add department', 'Exit']       
+    })
+    .then((answers)=> {
+        switch (answers.userOption){
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'View all departments':
+                //what to do
+                break;
+            case 'View all roles':
+                //what to do
+                break;
+            case 'Add employee':
+                //what to do
+                break;
+            case 'Add role':
+                //what to do
+                break;
+            case 'Add department':
+                //what to do
+                break;
+            case 'Exit':
+                connection.end()
+                break;
+        }
+    })
+}
 
-app.use(routes);
+const viewEmployees = () => {
+    app.get('/employee')
+}
 
 
-//start db connection
-connection.connect((err) => {
-    if (err) throw err;
-    startMenu()
-})
+
+//start connection
+connection.connect((err)=> {
+    if(err) throw err;
+    mainMenu();
+});
