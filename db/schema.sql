22db510e6e9e8b7492a7b4db790eb953a92377a9
Brandon King <brandonscottking@gmail.com>
2021-04-23 16:29:33 -0400

    updated file structure to focus solely on mysql, not sequelize

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,28 @@
 DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 
+CREATE TABLE department (
+    department_id int auto_increment,
+    name varchar(30),
+    primary key (department_id)
+);
+
+CREATE TABLE role (
+    role_id int auto_increment,
+    title varchar(30),
+    salary decimal,
+    department_id int,
+    primary key (role_id),
+    foreign key (department_id) references department (department_id)
+);
+
+CREATE TABLE employee (
+    id int auto_increment,
+    first_name varchar(30),
+    last_name varchar(30),
+    role_id int,
+    manager_id int,
+    primary key (id),
+    foreign key (role_id) references role (role_id),
+    foreign key (manager_id) references employee (id)
+);
